74ee4b9a9e2eac0cd011b60cb4782c4130bdfe9a
Ilyas Makhloul <127142801+ilyasMak@users.noreply.github.com>
2023-03-06 21:31:40 +0100

    Add files via upload

---
+++ ILYAS.js
@@ -0,0 +1,374 @@
+
+
+
+    function h(){
+            var home=document.getElementById("home").style.display="block" ;
+            var pers=document.getElementById("pers").style.display="none" ;
+            var prof=document.getElementById("prof").style.display="none" ;
+            var cnt=document.getElementById("CNTact").style.display="none " ;
+            var hm=document.getElementById("hmm") ;
+            var pr=document.getElementById("prr") ;
+            var prf=document.getElementById("prff") ;
+            var tt=document.getElementById("ttt") ;
+            var cntName=document.getElementById("cnt");
+            hm.style.textShadow="5px 12px 25px white";
+            pr.style.textShadow="none";
+            prf.style.textShadow="none";
+            tt.style.textShadow="none";
+            cntName.style.textShadow="none";
+
+        }
+
+
+
+        function pe(){
+           var pers=document.getElementById("pers").style.display="block" ;
+           var home=document.getElementById("home").style.display="none" ;
+           var prof=document.getElementById("prof").style.display="none" ;
+           var cnt=document.getElementById("CNTact").style.display="none " ;
+           var hm=document.getElementById("hmm") ;
+           var pr=document.getElementById("prr") ;
+           var prf=document.getElementById("prff") ;
+           var tt=document.getElementById("ttt") ;
+           var cntName=document.getElementById("cnt");
+                  pr.style.textShadow="5px 12px 25px white";
+                  hm.style.textShadow="none";
+                  prf.style.textShadow="none";
+                  tt.style.textShadow="none";
+                  cntName.style.textShadow="none";
+        }
+
+        function pr(){
+           var prof=document.getElementById("prof").style.display="block" ;
+           var pers=document.getElementById("pers").style.display="none" ;
+           var home=document.getElementById("home").style.display="none" ;
+           var cnt=document.getElementById("CNTact").style.display="none " ;
+            var hm=document.getElementById("hmm") ;
+            var prf=document.getElementById("prff") ;
+              var pr=document.getElementById("prr") ;
+              var tt=document.getElementById("ttt") ;
+              var cntName=document.getElementById("cnt");
+                     pr.style.textShadow="none";
+                     hm.style.textShadow="none";
+                     prf.style.textShadow="5px 12px 25px white";
+                     tt.style.textShadow="none";
+                     cntName.style.textShadow="none";
+        }
+
+           function t(){
+                  var prof=document.getElementById("prof").style.display="block" ;
+                  var pers=document.getElementById("pers").style.display="block" ;
+                  var cnt=document.getElementById("CNTact")
+                  cnt.style.display="block " ;
+                  //cnt.style.position="relative" ;
+                    //                        cnt.style.top="0rem " ;
+                  var home=document.getElementById("home").style.display="none" ;
+
+
+                        var pr=document.getElementById("prr") ;
+                          var hm=document.getElementById("hmm") ;
+                                     var prf=document.getElementById("prff") ;
+                                       var pr=document.getElementById("prr") ;
+                                       var tt=document.getElementById("ttt") ;
+                                       var cntName=document.getElementById("cnt");
+                                              pr.style.textShadow="5px 12px 25px white";
+                                              hm.style.textShadow="5px 12px 25px white";
+                                              prf.style.textShadow="5px 12px 25px white";
+                                              tt.style.textShadow="5px 12px 25px white";
+                                              cntName.style.textShadow="5px 12px 25px white";
+               }
+
+               function Contact(){
+                var home=document.getElementById("home").style.display="none" ;
+                          var pers=document.getElementById("pers").style.display="none" ;
+                          var prof=document.getElementById("prof").style.display="none" ;
+                          var cnt=document.getElementById("CNTact");
+                          cnt.style.display="block " ;
+                          var cntName=document.getElementById("cnt");
+                           cnt.style.position="relative" ;
+                          cnt.style.top="12rem " ;
+
+                          var hm=document.getElementById("hmm") ;
+                          var pr=document.getElementById("prr") ;
+                          var prf=document.getElementById("prff") ;
+                          var tt=document.getElementById("ttt") ;
+                          cntName.style.textShadow="5px 12px 25px white"
+                          pr.style.textShadow="none";
+                          prf.style.textShadow="none";
+                          tt.style.textShadow="none";
+
+
+               }
+
+
+
+function Active(){
+   var md1=document.getElementById("mode1");
+    var bd=document.getElementsByTagName("body");
+    var mn=document.getElementById("hmm");
+    var tt=document.getElementById("ttt") ;
+    var pr=document.getElementById("prr") ;
+    var prf=document.getElementById("prff") ;
+    //var fb=document.getElementById("f") ;
+   // var inst=document.getElementById("ins");
+   // var link=document.getElementById("in");
+   // var wtsp=document.getElementById("wtspp");
+    var hmImage=document.getElementById("1");
+    var prsImage=document.getElementById("2");
+    var prfImage=document.getElementById("3");
+    var ttImage=document.getElementById("4");
+    var md2=document.getElementById("mode2");
+    var nxt=document.getElementById("nxt");
+     var back=document.getElementById("back");
+     var det=document.getElementById("det");
+     var grn=document.getElementsByClassName("detPrg");
+     //document.getElementById("lglinkPC").src="C:/Users/pc/Pictures/me_files/in dr (2).png";
+     var ContImage=document.getElementById("5").src="C:/Users/pc/Pictures/me_files/let lite  (2).png";
+     var nmphn=document.getElementById("NamePhone").style.color="black";
+     var detbtn=document.getElementById("detb").style.color= "black" ;
+     var detbtn2=document.getElementById("detb2").style.color= "black" ;
+     var inIM=document.getElementById("lglink").src="C:/Users/pc/Pictures/me_files/in dr (2).png" ;
+      var infb=document.getElementById("lgfb").src="C:/Users/pc/Pictures/me_files/fb drk.png" ;
+var cntName=document.getElementById("cnt").style.color ="red";
+     var inML=document.getElementById("lgmail").src="C:/Users/pc/Pictures/me_files/1.png" ;
+      var iconsPhone=document.getElementsByClassName("11");
+      var ILlg=document.getElementById("profil").src="C:/Users/pc/Pictures/me_files/ilys lg (2).png";
+     iconsPhone[0].src ="C:/Users/pc/Pictures/me_files/hm bl.png";
+     iconsPhone[1].src ="C:/Users/pc/Pictures/me_files/prs lite.png";
+     iconsPhone[2].src ="C:/Users/pc/Pictures/me_files/prf lite.png" ;
+     iconsPhone[4].src ="C:/Users/pc/Pictures/me_files/tt lite.png";
+     iconsPhone[3].src ="C:/Users/pc/Pictures/me_files/let lite  (2).png";
+     iconsPhone[5].src =  "C:/Users/pc/Pictures/me_files/x (2).png";
+
+     for (i=0; i<grn.length; i++){
+       grn[i].style.background='white';
+     }
+     det.style.color="black";
+
+     //grn.style.background = "white";
+    //------------------------------------------------------
+    ttImage.src="C:/Users/pc/Pictures/me_files/tt lite.png";
+    prfImage.src="C:/Users/pc/Pictures/me_files/prf lite.png";
+    hmImage.src="C:/Users/pc/Pictures/me_files/hm bl.png" ;
+   prsImage.src="C:/Users/pc/Pictures/me_files/prs lite.png" ;
+   // wtsp.style.color = "black" ;
+   // link.style.color = "black" ;
+    //inst.style.color = "black" ;
+   // fb.style.color = "black" ;
+    hmm.style.color = "red" ;
+    pr.style.color = "red" ;
+    prf.style.color = "red" ;
+    tt.style.color = "red" ;
+    /*hmm.style.textDecoration ="underline" ;
+    pr.style.textDecoration ="underline" ;
+    prf.style.textDecoration ="underline" ;
+    tt.style.textDecoration ="underline" ;*/
+
+
+    //___________________________________________
+    document.body.style.color = "black";
+    document.body.style.background = "#B3C7DA";
+    md1.style.display="none";
+    md2.style.display="block";
+    //------------------------------------------
+   nxt.src="C:/Users/pc/Pictures/me_files/next.png";
+   back.src="C:/Users/pc/Pictures/me_files/back black.png" ;
+
+
+    }
+
+
+
+function Dative(){
+   var md1=document.getElementById("mode1");
+   var md2=document.getElementById("mode2");
+   var bd=document.getElementsByTagName("body");
+       var mn=document.getElementById("hmm");
+       var tt=document.getElementById("ttt") ;
+       var pr=document.getElementById("prr") ;
+       var prf=document.getElementById("prff") ;
+     //  var fb=document.getElementById("f") ;
+     //  var inst=document.getElementById("ins");
+     //  var link=document.getElementById("in");
+     //  var wtsp=document.getElementById("wtspp");
+       var hmImage=document.getElementById("1");
+       var prsImage=document.getElementById("2");
+       var prfImage=document.getElementById("3");
+       var ttImage=document.getElementById("4");
+       var ContImage=document.getElementById("5").src="C:/Users/pc/Pictures/me_files/letter  (2).png";
+       var nxt=document.getElementById("nxt");
+       var back=document.getElementById("back");
+       var det=document.getElementById("det");
+       var cntName=document.getElementById("cnt").style.color ="white";
+       var nmphn=document.getElementById("NamePhone").style.color="white";
+       var detbtn=document.getElementById("detb").style.color= "white" ;
+         var grn=document.getElementsByClassName("detPrg");
+         var ILlg=document.getElementById("profil").src="C:/Users/pc/Pictures/me_files/lg ily lt (2).png";
+         var inIM=document.getElementById("lglink").src="C:/Users/pc/Pictures/me_files/Sans titre (2).png" ;
+
+         var inML=document.getElementById("lgmail").src="C:/Users/pc/Pictures/me_files/mail red (2).png" ;
+         var infb=document.getElementById("lgfb").src="C:/Users/pc/Pictures/me_files/fb red.png" ;
+               var iconsPhone=document.getElementsByClassName("11");
+              iconsPhone[0].src ="C:/Users/pc/Pictures/me_files/hm (2).png";
+              iconsPhone[1].src ="C:/Users/pc/Pictures/me_files/prf (2).png";
+              iconsPhone[2].src ="C:/Users/pc/Pictures/me_files/dg (2).png" ;
+              iconsPhone[3].src ="C:/Users/pc/Pictures/me_files/letter  (2).png";
+              iconsPhone[4].src ="C:/Users/pc/Pictures/me_files/fl (2).png";
+              iconsPhone[5].src =  "C:/Users/pc/Pictures/me_files/frm drk (2).png";
+            for (i=0; i<grn.length; i++){
+              grn[i].style.background='black';
+            }
+            det.style.color="white";
+       //--------------------------------------------------------
+
+       //-------------------------------------------------------
+       back.src="C:/Users/pc/Pictures/me_files/back red.png"
+        nxt.src="C:/Users/pc/Pictures/me_files/nxt red.png";
+        ttImage.src="C:/Users/pc/Pictures/me_files/fl (2).png";
+           prfImage.src="C:/Users/pc/Pictures/me_files/dg (2).png";
+           hmImage.src="C:/Users/pc/Pictures/me_files/hm (2).png" ;
+          prsImage.src="C:/Users/pc/Pictures/me_files/prf (2).png" ;
+          //____________________________________________________________________
+       document.body.style.color = "white";
+        //wtsp.style.color = "white" ;
+        //   link.style.color = "white" ;
+        //   inst.style.color = "white" ;
+        //   fb.style.color = "white" ;
+           hmm.style.color = "white" ;
+           pr.style.color = "white" ;
+           prf.style.color = "white" ;
+           tt.style.color = "white" ;
+    md1.style.display="block";
+    md2.style.display="none";
+  document.body.style.background = "#15202B";
+  //-----------------------------------------------------------------------
+  hmm.style.textDecoration ="none" ;
+      pr.style.textDecoration ="none" ;
+      prf.style.textDecoration ="none" ;
+      tt.style.textDecoration ="none" ;
+
+      var detbtn2=document.getElementById("detb2").style.color= "white" ;
+
+
+
+
+}
+//_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+  function pasNxt(){
+
+
+
+    var ImgPrn=document.getElementById("cr1");
+    var Img2=document.getElementById("cr2");
+    var Img3=document.getElementById("cr3");
+    Imgs=[Img2.src,ImgPrn.src,Img3.src];
+    var tmp1=ImgPrn.src;
+    var tmp2=Img2.src;
+
+    ImgPrn.src=Img3.src ;
+    Img2.src=tmp1 ;
+    Img3.src=tmp2 ;
+    //transition="2s";*/
+
+  }
+
+
+  function pasback(){
+   var ImgPrn=document.getElementById("cr1");
+      var Img2=document.getElementById("cr2");
+      var Img3=document.getElementById("cr3");
+      Imgs=[Img2.src,ImgPrn.src,Img3.src];
+
+       var tmp1=ImgPrn.src;
+          var tmp2=Img3.src;
+
+          ImgPrn.src=Img2.src ;
+          Img3.src=tmp1 ;
+          Img2.src=tmp2 ;
+  }
+
+
+
+  function AffichDet(){
+
+
+    const info = document.querySelector('#det1');
+          info.classList.add('show');
+
+    document.getElementById("detb").style.display ="none" ;
+  }
+
+
+  function ReduireDet(){
+    const info = document.querySelector('#det1');
+             info.classList.remove('show');
+   document.getElementById("detb").style.display ="block" ;
+
+  }
+
+  function AffichPhoneMenu(){
+  var mn=document.getElementById("menuPh").style.display="block" ;
+  }
+
+  function frmMenuPh(){
+  var mn=document.getElementById("menuPh").style.display="none" ;
+  }
+
+
+
+
+function langueFRC(){
+document.getElementById("Im").innerHTML="I'm" ;
+document.getElementById("detb").innerHTML="Show more details" ;
+document.getElementById("detb2").innerHTML="Less details" ;
+   /*document.getElementById("frc").src="C:/Users/pc/Pictures/me_files/france.jpg" ;*/
+   document.getElementById("frc").style.display ="none ";
+   document.getElementById("ENG").style.display ="block ";
+   document.getElementById("hmm").innerHTML="Home" ;
+   document.getElementById("prr").innerHTML="About" ;
+   document.getElementById("prff").innerHTML="Professional";
+   document.getElementById("ttt").innerHTML="All";
+   document.getElementById("disc1").innerHTML= "Computer engineering student" ;
+   document.getElementById("disc2").innerHTML= "National School of Applied Science Fez" ;
+   var Cv=document.getElementsByClassName("cv");
+   Cv[0].innerHTML="Download CV"
+   document.getElementById("txtper").innerHTML="ILYAS MAKHLOUL , Student of the National School of Applied Sciences of Fez, in Computer Engineering, I really like the future of WEB development as well as programming in general (C/C++, JAVA, JavaScript,..), I always like to use logic to find fast, precise and automatic solutions. to avoid repeating a task, and that's exactly the concept of algorithms, that's why I'm a Programmer ."
+   document.getElementById("IP").innerHTML="Personal Information" ;
+   document.getElementById("IP1").innerHTML="First and Last Name : Ilyas Makhloul";
+   document.getElementById("IP2").innerHTML="Date of Birth: 17.10.2002";
+   document.getElementById("CM").innerHTML="Skills";
+   document.getElementById("titPrj").innerHTML="Project";
+     document.getElementById("ctfPHONE").innerHTML="Certificates" ;
+     document.getElementById("nomCont").innerHTML="Name" ;
+     document.getElementById("big").innerHTML="Write a message" ;
+      document.getElementById("ENV").innerHTML="Send" ;
+     //document.getElementByClassName("cnt").placeholder="Name" ;
+
+}
+
+
+function langueENG(){
+document.getElementById("frc").style.display ="block ";
+document.getElementById("detb").innerHTML="Plus des détails" ;
+document.getElementById("detb2").innerHTML="Moins de détails" ;
+   document.getElementById("ENG").style.display ="none ";
+   document.getElementById("hmm").innerHTML="Accueil" ;
+      document.getElementById("prr").innerHTML="personnelle" ;
+      document.getElementById("prff").innerHTML="professionnelle";
+      document.getElementById("ttt").innerHTML="Tous";
+       document.getElementById("disc1").innerHTML= "Etudiant Ingénieur en Génie Informatique" ;
+         document.getElementById("disc2").innerHTML= "Ecole Nationale de Science Appliquée fés" ;
+         var Cv=document.getElementsByClassName("cv");
+            Cv[0].innerHTML="Télecharger CV" ;
+            document.getElementById("txtper").innerHTML="ILYAS MAKHLOUL , Etudiant de l'ecole nationale de sciences appliquées de fes, en Génie Informatique, j'aime beaucoup le demaine de devlleppement WEB ainsi la programmation en génrale (C/C++, JAVA, JavaScript,..),J'ai toujours aimé d'utiliser la logique pour trouver des solutions rapides, précises et automatiques.pour éviter de repeter une tach , et c'est exactement le concept d'algorithmes ,c'est poour cela je suis Programmere . ";
+            document.getElementById("IP").innerHTML="Information Personelle" ;
+               document.getElementById("IP1").innerHTML="Nom et Prenom :Ilyas Makhloul";
+               document.getElementById("IP2").innerHTML="Date de Naissance : 17.10.2002";
+               document.getElementById("CM").innerHTML="Compétences";
+               document.getElementById("titPrj").innerHTML="Projet";
+                 document.getElementById("ctfPHONE").innerHTML="Les Certificats"
+                 document.getElementById("Im").innerHTML="Je Suis" ;
+                  document.getElementById("nomCont").innerHTML="Nom" ;
+                  document.getElementById("big").innerHTML="Ecrire un Message" ;
+                  document.getElementById("ENV").innerHTML="Envoyer" ;
+}
